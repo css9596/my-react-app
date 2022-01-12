656b4170ee493c6891bb9d38350fd83928b16e17
Choi Sung Sik <css9596@naver.com>
2022-01-12 22:47:33 +0900

    state 가 변경이 되면 React는 변화를 감지하고 컴포넌트를 랜더링 하여 계속해서 새로고침을 한다.
    이때 랜더링 처리를 안하고 싶은 코드가 있을 때에는 useEffect를 사용하면 된다!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 
+import {useState} from "react";
+
 function App() {
+    const [counter, setValue] = useState(0);
+    const onClick = () => {
+        setValue((prev) => prev + 1);
+    }
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-        <Button text={"Continue"}/>
+      <h1 className={styles.title}>{counter}</h1>
+        <Button text={"Continue"} onClick={onClick}/>
     </div>
   );
 }
