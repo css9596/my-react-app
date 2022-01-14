6b75400154adfbaa00de5c053dc65525e259034d
Choi Sung Sik <css9596@naver.com>
2022-01-15 00:28:18 +0900

    useEffect가 한번 실행하는 것을 배운 뒤, useEffect의 2번째 인자 값에 state value를 지정하면 해당 state가 변경이 되었을 때 만 실행한다.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import {useState, useEffect} from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
     const onClick = () => {
         setValue((prev) => prev + 1);
     }
-    console.log("i run all the ");
+    const onChange = (event) => {
+        setKeyword(event.target.value);
+    }
     const iRunOnlyOnce = () => {
         console.log("i run only once.");
     }
@@ -16,9 +19,15 @@ function App() {
     useEffect(() => {
         console.log("CALL THE API")
     }, []);
+    useEffect(() => {
+        if(keyword !== "" && keyword.length > 5 ){
+            console.log("SEARCH FOR", keyword);
+        }
+    }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>{counter}</h1>
+        <input type="text" placeholder="Search here." onChange={onChange} />
+        <h1 className={styles.title}>{counter}</h1>
         <button onClick={onClick}>Click</button>
     </div>
   );
